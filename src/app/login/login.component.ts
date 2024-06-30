71b23c5d73fa47cf6d4cd27fd2482dccddebf8a1
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-06-30 18:03:43 +0200

    dashboard and login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit() {
+    this.authService.login(this.username, this.password).subscribe(
+      success => {
+        if (success) {
+          this.router.navigate(['/dashboard']);
+        } else {
+          alert('Login failed');
+        }
+      },
+      error => {
+        console.error(error);
+        alert('An error occurred during login');
+      }
+    );
+  }
 }
