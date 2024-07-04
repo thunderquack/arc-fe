61d0f65e7672a6183e37c88a16a5264c83dde386
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-07-04 18:29:09 +0200

    backend reference

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<boolean> {
-    return this.http.post<{ token: string }>('http://localhost:3000/api/login', { username, password })
+    return this.http.post<{ token: string }>('http://arc-be:3000/api/login', { username, password })
       .pipe(
         map(response => {
           if (response.token) {
