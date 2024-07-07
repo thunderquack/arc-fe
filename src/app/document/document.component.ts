32a16b15e14e9f673e95235828ac78fc6ae7600f
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-07-07 18:49:42 +0200

    customized document component

--- src/app/document/document.component.ts
+++ src/app/document/document.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-document',
   templateUrl: './document.component.html',
-  styleUrl: './document.component.css'
+  styleUrls: ['./document.component.css']
 })
-export class DocumentComponent {
+export class DocumentComponent implements OnInit {
+  documentTitle: string = '';
+  creator: string = '';
+  date: string = '';
+  summary: string = '';
+  recognizedText: string = '';
+  pages: any[] = [];
+  selectedPage: any;
 
+  ngOnInit(): void {
+    // Инициализация данных, загрузка документа и страниц
+  }
+
+  selectPage(page: any): void {
+    this.selectedPage = page;
+  }
 }
